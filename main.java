f4b71c7395a13ae8e3d1724e7f52aeb0937dd287
eamonn-carr <70214167+eamonn-carr@users.noreply.github.com>
2020-08-27 19:58:59 +0100

    Add files via upload

---
+++ main.java
@@ -0,0 +1,19 @@
+package brick;
+
+import javax.swing.JFrame;
+
+public class main {
+
+	public static void main(String[] args) {
+		JFrame obj = new JFrame(); 
+		Gameplay gamePlay = new Gameplay();
+		obj.setBounds(10,19,700,600);
+		obj.setTitle("Breakout Ball");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gamePlay);
+	}
+	
+
+}
